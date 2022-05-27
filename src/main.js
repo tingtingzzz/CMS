d2dc75ae4e7136015e188f73ca8430b1316779a4
张嘉文 <65259423@qq.com>
2022-05-27 10:14:57 +0800

    cms项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入axios
+import axios from 'axios'
+//引入 ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//设置根路径
+axios.defaults.baseURL = 'http://www.liulongbin.top:3008'
+//将axios挂载到 vue构造函数的原型对象上 prototype
+Vue.prototype.$http = axios
+//注册 ElementUI
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
